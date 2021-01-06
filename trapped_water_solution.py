6d39c1fbf87d668581a9d57731cd46187021fd35
ARPIT SHUKLA <47503958+arpitabhi@users.noreply.github.com>
2021-01-07 02:08:16 +0530

    Update trapped_water_solution.py

--- trapped_water_solution.py
+++ trapped_water_solution.py
@@ -1,3 +1,10 @@
+#	Rain Water Trapped
+#	Problem Description
+
+#	Given a vector A of non-negative integers representing an elevation map where the width of each bar is 1, 
+#	compute how much water it is able to trap after raining.
+
+
 class Solution:
 	# @param A : tuple of integers
 	# @return an integer
