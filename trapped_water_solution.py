0d6f4085f18cb6430d92aaf80e302c6040a4f921
ARPIT SHUKLA <47503958+arpitabhi@users.noreply.github.com>
2021-01-07 02:06:58 +0530

    Add files via upload

---
+++ trapped_water_solution.py
@@ -0,0 +1,29 @@
+class Solution:
+	# @param A : tuple of integers
+	# @return an integer
+	def trap(self, A):
+	    water_depth=[]
+	    left_max=[]
+        right_max=[]
+	    max_left=A[0]
+	    max_right=A[-1]
+        for i in A:
+            if i>max_left:
+                max_left=i
+	        left_max.append(max_left)
+	        
+        for j in range(len(A)-1,-1,-1):
+	        if A[j]>max_right:
+	            max_right=A[j]
+	        right_max.append(max_right)
+	   
+	    right_max.reverse()
+	    
+        for i in range(1,len(A)-1):
+            x=min(left_max[i-1],right_max[i+1])-A[i]
+            if x>0:
+                water_depth.append(x)
+        return sum(water_depth)
+        
+        
+            
